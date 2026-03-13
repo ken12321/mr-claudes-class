10d2f398fc65b59c0b161a4e394e0bf11efddae7
Ken Hetherington <ken.l.h23@gmail.com>
2026-03-13 18:34:51 +1100

    Fixing bugs

--- sphere.h
+++ sphere.h
@@ -16,17 +16,17 @@ struct Sphere : public Hittable
 
     std::optional<HitRecord> hit(const Ray& ray) const override
     {
-        float a = dot(ray.getDirection(), ray.getDirection());
-        float b = 2 * dot(ray.getDirection(), ray.getOrigin() - center) - (radius*radius);
-        float c = dot(ray.getOrigin() - center, ray.getOrigin() - center) - (radius*radius);
+        double a = dot(ray.getDirection(), ray.getDirection());
+        double b = 2 * dot(ray.getDirection(), ray.getOrigin() - center);
+        double c = dot(ray.getOrigin() - center, ray.getOrigin() - center) - (radius*radius);
 
-        float discriminant = (b*b) - 4*(a*c);
+        double discriminant = (b*b) - 4*(a*c);
         if (discriminant < 0)
         {
             return std::nullopt;
         }
 
-        float t = (-b - std::sqrt(discriminant)) / (a*a);
+        double t = (-b - std::sqrt(discriminant)) / (2*a);
         if (t > 0)
         {
             Vec3d point = ray.at(t);
